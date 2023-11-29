22a20218cee27145b878a836d9f03c2d1f1f8853
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-11-30 00:14:22 +0300

    remove trash

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import SignIn from './components/Popups/WithForm/SignIn/SignIn';
 import './index.scss';
 
 function App() {
     return (
         <>
             <div>HOME</div>
-            <SignIn></SignIn>
         </>
     );
 }
