f3cab6b9f5910c1b634309be81c4975eab78f509
Roman Anurov <anurovroman@yandex.ru>
2023-12-04 17:08:24 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import PasswordSaved from './components/Popups/InfoPopup/PasswordSaved/PasswordSaved';
 import PopupSignupSuccess from './components/Popups/InfoPopup/PopupSignupSuccess/PopupSignupSuccess';
+import SignIn from './components/Popups/WithForm/SignIn/SignIn';
 import SignUp from './components/Popups/WithForm/SignUp/SignUp';
 import './index.scss';
 
@@ -7,6 +8,7 @@ function App() {
     return (
         <>
             <div>HOME</div>
+            <SignIn/>
             <SignUp />
             <PopupSignupSuccess />
             <PasswordSaved />
