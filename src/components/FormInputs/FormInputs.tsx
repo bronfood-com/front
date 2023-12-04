745808a10281d72bfc27ec979787494c31fbd959
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-12-04 21:30:13 +0300

    add jsdoc

--- src/components/FormInputs/FormInputs.tsx
+++ src/components/FormInputs/FormInputs.tsx
@@ -1,5 +1,13 @@
 import { FC, ReactNode } from 'react';
 import styles from './FormInputs.module.scss';
+/**
+ *
+ * This block 'FormInputs' adds gap between inputs,
+ * !!! use it only for inputs, wrap inputs !!!
+ * Title h2 margin will not depend by input and
+ * it will have its own margin-bottom 30px(which is needed in almost all popups).
+ * Input has its own margin-top 8px cause of lable above.
+ *  */
 
 interface FormInputs {
     children?: ReactNode;
