56ff2d3f4cc24ae8a8bc40807ef4f652d5656406
Roman Anurov <anurovroman@yandex.ru>
2023-12-04 23:28:54 +0300

    save

--- src/components/Popups/InfoPopup/PopupSignupSuccess/PopupSignupSuccess.tsx
+++ src/components/Popups/InfoPopup/PopupSignupSuccess/PopupSignupSuccess.tsx
@@ -1,12 +1,16 @@
 import Popup from '../../Popup/Popup';
 import InfoImage from '../../../InfoImage/InfoImage';
+import styles from "./PopupSignupSuccess.module.scss";
 
 const PopupSignupSuccess = () => {
     return (
-        <Popup title="Спасибо за регистрацию!" mode="info">
+        <Popup mode="info" >
+            <h2 className={styles.popup__title}>Спасибо за регистрацию!</h2>
             <InfoImage mode="red_tube" />
         </Popup>
     );
 };
 
 export default PopupSignupSuccess;
+
+
