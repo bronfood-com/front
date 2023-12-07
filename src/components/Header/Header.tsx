ec2e023f9d89ae467722bc853acc76f73c4d4523
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-12-07 22:44:19 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,30 +3,30 @@ import Navigation from '../Navigation/Navigation';
 import styles from './Header.module.scss';
 
 const Header = () => {
-    const [isMenuActive, SetIsMenuActive] = useState(false);
+    const [isMenuActive, setIsMenuActive] = useState(false);
     const handleMenuActive = () => {
-        SetIsMenuActive(!isMenuActive);
+        setIsMenuActive(!isMenuActive);
     };
 
     return (
         <header className={styles.header}>
             <div className={styles.header__container}>
-                <button className={`${styles.header__burger} ${styles.header__icon} button`} onClick={handleMenuActive}></button>
+                <button title='Меню' className={`${styles.header__burger} ${styles.header__icon} button`} onClick={handleMenuActive}></button>
                 <div className={styles.header__place}>
                     <div className={styles.header__place_point}></div>
                     <p className={styles.header__place_name}>Алматы</p>
                 </div>
                 <div className={styles.header__buttons}>
-                    <button className={`${styles.header__favorite} ${styles.header__icon} button`}></button>
-                    <button className={`${styles.header__search} ${styles.header__icon} button`}></button>
+                    <button title='Избранное' className={`${styles.header__favorite} ${styles.header__icon} button`}></button>
+                    <button title='Поиск' className={`${styles.header__search} ${styles.header__icon} button`}></button>
                 </div>
             </div>
             <div className={`${styles.header__menu} ${isMenuActive ? styles.header__menu_active : ''}`}>
                 <div className={styles.header__upblock}>
                     <a className={styles.header__logo} href="#"></a>
-                    <button className={`${styles.header__close} ${styles.header__icon} button`} onClick={handleMenuActive}></button>
+                    <button title='Закрыть' className={`${styles.header__close} ${styles.header__icon} button`} onClick={handleMenuActive}></button>
                 </div>
-                <Navigation></Navigation>
+                <Navigation />
             </div>
         </header>
     );
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
 import styles from './Navigation.module.scss';
 import GuestNavigation from './GuestNavigation/GuestNavigation';
 import CustomerNavigation from './CustomerNavigation/CustomerNavigation';
-import CateringNavigation from './CateringNavigation/CateringNavigation';
-import CashierNavigation from './CashierNavigation/CashierNavigation';
 
 /**
  * Contains 4 menu types. For change --> set useState on TRUE (guest, client, catering, cashier)
  */
 const Navigation = () => {
-    const [isGuest] = useState(true);
-    const [isCustomer] = useState(false);
-    const [isCatering] = useState(false);
-    const [isCashier] = useState(false);
 
-    return <nav className={styles.nav}>{isGuest ? <GuestNavigation /> : isCustomer ? <CustomerNavigation /> : '' || isCatering ? <CateringNavigation /> : '' || isCashier ? <CashierNavigation /> : ''}</nav>;
+    type UserRole = 'guest' | 'customer' | 'catering';
+    const userRole: UserRole = 'customer';
+
+    const renderNavigation = (userRole: UserRole) => {
+        if (userRole === 'guest') {
+            return <GuestNavigation />;
+        } else {
+            return <CustomerNavigation />;
+        }
+    };
+
+    return <nav className={styles.nav}>{renderNavigation(userRole)}</nav>;
 };
 
 export default Navigation;
