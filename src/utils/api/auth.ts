520b76b1c4bfaef5e9ad79deae1415b53227ba2b
Vitkor <vitland@gmail.com>
2023-12-14 22:39:08 +0300

    feat: update api

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,62 +1,27 @@
-type Data = {
-    phone?: string;
-    name?: string;
-    password?: string;
+interface LoginData {
+    phone: string;
+    password: string;
+}
+
+interface User {
+    phone: string;
+    name: string;
     isCatering: boolean;
-};
-
+}
 class AuthApi {
-    async login({ phone, password }: Data): Promise<{ status: string; data?: string; message?: string }> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                if (phone && password) {
-                    resolve({ status: 'success', data: 'data' });
-                } else {
-                    resolve({ status: 'error', message: 'error' });
-                }
-            }, 500);
-        });
+    async _wait(ms: number) {
+        return new Promise((res) => setTimeout(res, ms));
     }
 
-    async register({ name, phone, password, isCatering }: Data): Promise<{ status: string; data?: string; message?: string }> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                if (name && phone && password && isCatering) {
-                    resolve({ status: 'success', data: 'data' });
-                } else {
-                    resolve({ status: 'error', message: 'error' });
-                }
-            }, 500);
-        });
+    async login({ phone, password }: LoginData): Promise<{ status: 'success' | 'error'; data?: User; message?: string }> {
+        await this._wait(500);
+        if (phone && password) {
+            return { status: 'success', data: { name: 'User', phone: '111', isCatering: false } };
+        } else {
+            return { status: 'error', message: 'error' };
+        }
     }
 
-    async loguOut(): Promise<{ status: string }> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve({ status: 'success' });
-            }, 500);
-        });
-    }
-
-    async getUser(): Promise<{ user: string }> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve({ user: 'success' });
-            }, 500);
-        });
-    }
-
-    async updateUser({ name, phone, password }: Data): Promise<{ status: string; data?: string; message?: string }> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                if (name && phone && password) {
-                    resolve({ status: 'success' });
-                } else {
-                    resolve({ status: 'error', message: 'error' });
-                }
-            }, 500);
-        });
-    }
 }
 
 export const authApi = new AuthApi();
