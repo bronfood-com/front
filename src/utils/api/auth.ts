3b5fa4a31b594d6695c339db62d88d39c3dfce78
Kikodze Viktor <vitland-x@yandex.ru>
2023-12-16 23:17:42 +0300

    feat: add new type

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,27 +1,30 @@
+/**
+ * Phone number. Begins with '+' followed by 11 or more digits. Must not contain spaces or parentheses (или как вы договоритесь с бэками о формате)
+ */
+type PhoneNumber = string;
+
 interface LoginData {
-    phone: string;
+    phone: PhoneNumber;
     password: string;
 }
 
 interface User {
     phone: string;
     name: string;
-    isCatering: boolean;
 }
 class AuthApi {
     async _wait(ms: number) {
         return new Promise((res) => setTimeout(res, ms));
     }
 
-    async login({ phone, password }: LoginData): Promise<{ status: 'success' | 'error'; data?: User; message?: string }> {
+    async login({ phone, password }: LoginData): Promise<{ status: 'success' | 'error'; data?: User; errorMessage?: string }> {
         await this._wait(500);
         if (phone && password) {
-            return { status: 'success', data: { name: 'User', phone: '111', isCatering: false } };
+            return { status: 'success', data: { name: 'User', phone: phone} };
         } else {
-            return { status: 'error', message: 'error' };
+            return { status: 'error', errorMessage: 'error' };
         }
     }
-
 }
 
 export const authApi = new AuthApi();
