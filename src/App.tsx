0c3f5b1c896302225b41b675e93cdaddbe5a5e8b
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-12-17 16:49:40 +0300

    del garbage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import SignUp from './components/Popups/WithForm/SignUp/SignUp';
 import './index.scss';
 
 function App() {
     return (
         <>
             <div>HOME</div>
-            <SignUp></SignUp>
         </>
     );
 }
