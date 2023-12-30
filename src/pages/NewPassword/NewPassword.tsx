83c4520d41904513b5872bbeecd845bb6c9b27f2
Vitkor <vitland@gmail.com>
2023-12-30 19:40:51 +0300

    feat: add validation

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -5,19 +5,29 @@ import FormInputs from '../../components/FormInputs/FormInputs';
 import Input from '../../components/Input/Input';
 import Popup from '../../components/Popups/Popup/Popup';
 import { useTranslation } from 'react-i18next';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { regexPassword } from '../../utils/consts';
 
 const NewPassword = () => {
     const { t } = useTranslation();
     const navigate = useNavigate();
-    const onSubmit = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit: SubmitHandler<FieldValues> = async () => {
         navigate('/password_done');
+
     };
+
     return (
         <Popup title={t('pages.newPassword.title')}>
-            <Form name="form-password-new" onSubmit={onSubmit}>
+            <Form name="form-password-new" onSubmit={handleSubmit(onSubmit)}>
                 <FormInputs>
-                    <Input type="text" name="newPassword" placeholder="******" nameLabel={t('pages.newPassword.nameLabel')}></Input>
-                    <Input type="text" name="newPasswordDouble" placeholder="******" nameLabel={t('pages.newPassword.nameLabelRepeat')}></Input>
+                    <Input type="text" name="newPassword" placeholder="123456" nameLabel={t('pages.newPassword.nameLabel')}  register={register} errors={errors} pattern={regexPassword}></Input>
+                    <Input type="text" name="newPasswordDouble" placeholder="123456" nameLabel={t('pages.newPassword.nameLabelRepeat')}  register={register} errors={errors} pattern={regexPassword}></Input>
                 </FormInputs>
                 <Button>{t('pages.newPassword.button')}</Button>
             </Form>
