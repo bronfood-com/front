c08933f605337c9f3217d1854329b64f2388c635
Olga Smith <ms.olgasmith.jj@mail.ru>
2024-01-08 17:33:36 +0300

    add password-repeat validation

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -6,7 +6,7 @@ import Input from '../../components/Input/Input';
 import Popup from '../../components/Popups/Popup/Popup';
 import styles from './Profile.module.scss';
 import { useTranslation } from 'react-i18next';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 import { regexClientName, regexPassword } from '../../utils/consts';
 import InputPhone from '../../components/InputPhone/InputPhone';
 const Profile = () => {
@@ -14,21 +14,16 @@ const Profile = () => {
         register,
         handleSubmit,
         formState: { errors },
-        setError,
         getValues,
     } = useForm();
     const { t } = useTranslation();
     const navigate = useNavigate();
     const onSubmit = () => {
-        const { password, passwordRepeat } = getValues();
-        if (password !== passwordRepeat) {
-            setError('passwordRepeat', {
-                type: 'manual',
-                message: 'Неверный ввод',
-            });
-            return;
-        }
         navigate('/');
+    };
+    const validatePasswordMatch = (value:FieldValues) => {
+        const {password} = getValues()
+        return password === value || t('pages.newPassword.passwordDontMatch')
     };
     return (
         <Popup title={t('pages.profile.title')}>
@@ -37,7 +32,7 @@ const Profile = () => {
                     <Input type="text" name="username" placeholder={t('pages.profile.placeholderUserName')} nameLabel={t('pages.profile.nameLabelUserName')} register={register} errors={errors} pattern={regexClientName}></Input>
                     <InputPhone register={register} errors={errors}></InputPhone>
                     <Input type="password" name="password" placeholder='******' nameLabel={t('pages.profile.nameLabelPassword')} register={register} errors={errors} pattern={regexPassword}></Input>
-                    <Input type="password" name="passwordRepeat" placeholder='******' nameLabel={t('pages.profile.nameLabelRepeatPassword')} register={register} errors={errors} pattern={regexPassword}></Input>
+                    <Input type="password" name="passwordRepeat" placeholder='******' nameLabel={t('pages.profile.nameLabelRepeatPassword')} register={register} errors={errors} pattern={regexPassword} validate={validatePasswordMatch}></Input>
                 </FormInputs>
                 <div className={styles.profile__button_space}></div>
                 <Button>{t('pages.profile.continue')}</Button>
