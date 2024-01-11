3762897d3dffc1f7b248c86e411768e4d2403054
Kikodze Viktor <vitland-x@yandex.ru>
2024-01-11 23:31:37 +0300

    feat: add InputPassword component

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -2,22 +2,28 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Form from '../../components/Form/Form';
 import FormInputs from '../../components/FormInputs/FormInputs';
-import Input from '../../components/Input/Input';
 import Popup from '../../components/Popups/Popup/Popup';
 import { useTranslation } from 'react-i18next';
+import { useForm } from 'react-hook-form';
+import InputPassword from '../../components/InputPassword/InputPassword';
 
 const NewPassword = () => {
     const { t } = useTranslation();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
     const navigate = useNavigate();
     const onSubmit = () => {
         navigate('/password_done');
     };
     return (
         <Popup title={t('pages.newPassword.title')}>
-            <Form name="form-password-new" onSubmit={onSubmit}>
+            <Form name="form-password-new" onSubmit={handleSubmit(onSubmit)}>
                 <FormInputs>
-                    <Input type="text" name="newPassword" placeholder="******" nameLabel={t('pages.newPassword.nameLabel')}></Input>
-                    <Input type="text" name="newPasswordDouble" placeholder="******" nameLabel={t('pages.newPassword.nameLabelRepeat')}></Input>
+                    <InputPassword register={register} errors={errors} name="newPassword" nameLabel={t('pages.newPassword.nameLabel')} />
+                    <InputPassword register={register} errors={errors} name="newPasswordDouble" nameLabel={t('pages.newPassword.nameLabelRepeat')} />
                 </FormInputs>
                 <Button>{t('pages.newPassword.button')}</Button>
             </Form>
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -6,21 +6,29 @@ import Input from '../../components/Input/Input';
 import Popup from '../../components/Popups/Popup/Popup';
 import styles from './Profile.module.scss';
 import { useTranslation } from 'react-i18next';
+import { useForm } from 'react-hook-form';
+import InputPassword from '../../components/InputPassword/InputPassword';
 const Profile = () => {
 
     const { t } = useTranslation();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
     const navigate = useNavigate();
     const onSubmit = () => {
         navigate('/');
     };
+    
     return (
         <Popup title={t('pages.profile.title')}>
-            <Form name="form-profile" onSubmit={onSubmit}>
+            <Form name="form-profile" onSubmit={handleSubmit(onSubmit)}>
                 <FormInputs>
                     <Input type="text" name="username" placeholder={t('pages.profile.placeholderUserName')} nameLabel={t('pages.profile.nameLabelUserName')}></Input>
                     <Input type="number" name="telephone" placeholder={t('pages.profile.placeholderPhone')} nameLabel={t('pages.profile.nameLabelPhone')}></Input>
-                    <Input type="password" name="password" placeholder='******' nameLabel={t('pages.profile.nameLabelPassword')}></Input>
-                    <Input type="password" name="password" placeholder='******' nameLabel={t('pages.profile.nameLabelRepeatPassword')}></Input>
+                    <InputPassword register={register} errors={errors} name="newPassword" nameLabel={t('pages.newPassword.nameLabel')} />
+                    <InputPassword register={register} errors={errors} name="newPasswordDouble" nameLabel={t('pages.newPassword.nameLabelRepeat')} />
                 </FormInputs>
                 <div className={styles.profile__button_space}></div>
                 <Button>{t('pages.profile.continue')}</Button>
