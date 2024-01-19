4c5d1ba4a33d29382519845d2ab7906c363bc51a
Olga Smith <ms.olgasmith.jj@mail.ru>
2024-01-19 17:46:47 +0300

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: process.env.NODE_ENV === 'production' ? '/front/' : '',
+    base: process.env.NODE_ENV === 'production' ? '/front/pr-preview/pr-40/' : '',
+
 });
 
