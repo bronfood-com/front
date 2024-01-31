456c9543b37697ec22ce114d1dd4a9c6e7276645
Roman Anurov <anurovroman@yandex.ru>
2024-01-31 22:54:29 +0300

    fix: Preloader SignIn PasswordRecavery NewPassword

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import styles from './NewPassword.module.scss';
 import Button from '../../components/Button/Button';
 import Form from '../../components/Form/Form';
 import FormInputs from '../../components/FormInputs/FormInputs';
@@ -6,8 +8,10 @@ import Popup from '../../components/Popups/Popup/Popup';
 import { useTranslation } from 'react-i18next';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import InputPassword from '../../components/InputPassword/InputPassword';
+import Preloader from '../../components/Preloader/Preloader';
 
 const NewPassword = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const { t } = useTranslation();
     const navigate = useNavigate();
     const {
@@ -18,7 +22,9 @@ const NewPassword = () => {
     } = useForm();
 
     const onSubmit: SubmitHandler<FieldValues> = async () => {
-        navigate('/password_done');
+        setIsLoading(true);
+        navigate('');
+        setIsLoading(false);
     };
     const validatePasswordMatch = (value: FieldValues) => {
         const { newPassword } = getValues();
@@ -27,12 +33,15 @@ const NewPassword = () => {
 
     return (
         <Popup title={t('pages.newPassword.title')}>
+             {isLoading && <Preloader />}
             <Form name="form-password-new" onSubmit={handleSubmit(onSubmit)}>
+            <fieldset className={styles.form__field} disabled={isLoading}>
                 <FormInputs>
                     <InputPassword register={register} errors={errors} name="newPassword" nameLabel={t('pages.newPassword.nameLabel')} />
                     <InputPassword register={register} errors={errors} name="newPasswordDouble" nameLabel={t('pages.newPassword.nameLabelRepeat')} validate={validatePasswordMatch} />
                 </FormInputs>
-                <Button>{t('pages.newPassword.button')}</Button>
+                </fieldset>
+                <Button disabled={isLoading}>{t('pages.newPassword.button')}</Button>
             </Form>
         </Popup>
     );
--- src/pages/PasswordRecovery/PasswordRecovery.tsx
+++ src/pages/PasswordRecovery/PasswordRecovery.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import styles from './PasswordRecovery.module.scss';
 import Button from '../../components/Button/Button';
 import Form from '../../components/Form/Form';
 import FormInputs from '../../components/FormInputs/FormInputs';
@@ -6,8 +8,10 @@ import Popup from '../../components/Popups/Popup/Popup';
 import { useTranslation } from 'react-i18next';
 import { useForm } from 'react-hook-form';
 import InputPhone from '../../components/InputPhone/InputPhone';
+import Preloader from '../../components/Preloader/Preloader';
 
 const PasswordRecovery = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const {
         register,
         handleSubmit,
@@ -16,15 +20,21 @@ const PasswordRecovery = () => {
     const { t } = useTranslation();
     const navigate = useNavigate();
     const onSubmit = () => {
+        setIsLoading(true);
         navigate('/new_pass');
+        setIsLoading(false);
+
     };
     return (
         <Popup title={t('pages.passwordRecovery.title')}>
+             {isLoading && <Preloader />}
             <Form name="form-password-recovery" onSubmit={handleSubmit(onSubmit)}>
+            <fieldset className={styles.form__field} disabled={isLoading}>
                 <FormInputs>
                     <InputPhone register={register} errors={errors}></InputPhone>
                 </FormInputs>
-                <Button>{t('pages.passwordRecovery.continue')}</Button>
+                </fieldset>
+                <Button disabled={isLoading}>{t('pages.passwordRecovery.continue')}</Button>
             </Form>
         </Popup>
     );
