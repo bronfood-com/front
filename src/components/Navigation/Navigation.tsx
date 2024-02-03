436568a563c245f0deafd912d322262071fda2bf
Kikodze Viktor <vitland-x@yandex.ru>
2024-02-03 12:36:56 +0300

    feat: get state from context

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 import GuestNavigation from './GuestNavigation/GuestNavigation';
 import CustomerNavigation from './CustomerNavigation/CustomerNavigation';
+import { useCurrentUser } from '../../utils/hooks/useCurrentUser/useCurretUser';
 import { FC } from 'react';
-
 /**
  * Contains 2 menu types: guest/customer
  */
@@ -10,14 +10,11 @@ interface Navigation {
      * Click on menu item redirects to link and close menu
      */
     handleItemMenuClick: React.MouseEventHandler<HTMLElement>;
-    /**
-     * Login status. When true, the user is logged in
-     */
-    isLogin: boolean;
 }
 const Navigation: FC<Navigation> = (props) => {
+    const { isLogin } = useCurrentUser();
     type UserRole = 'guest' | 'customer' | 'catering';
-    const userRole: UserRole = props.isLogin ? 'customer' : 'guest';
+    const userRole: UserRole = isLogin ? 'customer' : 'guest';
     const navigation = userRole === 'guest' ? <GuestNavigation handleItemMenuClick={props.handleItemMenuClick} /> : <CustomerNavigation handleItemMenuClick={props.handleItemMenuClick} />;
     return <nav> {navigation} </nav>;
 };
