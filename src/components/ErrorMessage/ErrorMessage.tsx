099d157752b189d81ae3ef9de6ea88566d6a43af
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-05 20:56:31 +0300

    error component layout

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -6,12 +6,10 @@ const ErrorMessage: FC = () => {
         <div className={styles.error}>
             <div className={styles.error__container}>
                 <div className={styles.error__icon} />
-                <p className={styles.error__text}>
-                    Телефон или пароль введен неверно, повторите попытку еще раз.
-                </p>
+                <p className={styles.error__text}>Телефон или пароль введен неверно, повторите попытку еще раз.</p>
             </div>
         </div>
-    )
+    );
 };
 
 export default ErrorMessage;
