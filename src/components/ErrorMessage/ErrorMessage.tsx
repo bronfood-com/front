c499d52d47972a10c487f858acc4f25349e46f7b
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-05 20:55:42 +0300

    error component layout

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -2,7 +2,16 @@ import { FC } from 'react';
 import styles from './ErrorMessage.module.scss';
 
 const ErrorMessage: FC = () => {
-    return <div className={`${styles.error}`}>gkjygkyg</div>;
+    return (
+        <div className={styles.error}>
+            <div className={styles.error__container}>
+                <div className={styles.error__icon} />
+                <p className={styles.error__text}>
+                    Телефон или пароль введен неверно, повторите попытку еще раз.
+                </p>
+            </div>
+        </div>
+    )
 };
 
 export default ErrorMessage;
