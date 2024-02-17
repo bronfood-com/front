02bd4ba72f25e3a2986429ea45ca4dc91c8e86f3
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-17 18:25:29 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { CurrentUserProvider } from './contexts/CurrentUserContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/front/' : '/'}>
+        <BrowserRouter>
             <CurrentUserProvider>
                 <App />
             </CurrentUserProvider>
