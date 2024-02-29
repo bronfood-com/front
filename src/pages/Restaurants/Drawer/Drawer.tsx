2c328d1b3f44e677b1e86c6103d580687b4e0f5c
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-29 23:08:00 +0300

    fix PR57

--- src/pages/Restaurants/Drawer/Drawer.tsx
+++ src/pages/Restaurants/Drawer/Drawer.tsx
@@ -1,26 +1,21 @@
+import { useRef } from 'react';
 import RestaurantCard from '../../../components/Cards/RestaurantCard/RestaurantCard';
 import styles from './Drawer.module.scss';
 import { Restaurant } from '../MockRestaurantsList';
-import { useRef } from 'react';
-
-enum DrawerDirection {
-    up = 'up',
-}
 
 type DrawerProps = {
     isOpen: boolean;
     title: string;
     list: Restaurant[];
-    direction?: DrawerDirection;
     toggleDrawer: () => void;
     openFilter: () => void;
     openListItem: () => void;
 };
 
-const Drawer = ({ isOpen, title, list, direction = DrawerDirection.up, toggleDrawer, openFilter, openListItem }: DrawerProps) => {
+const Drawer = ({ isOpen, title, list, toggleDrawer, openFilter, openListItem }: DrawerProps) => {
     const container = useRef(null);
     return (
-        <div className={`${styles.drawer} ${styles[direction]} ${isOpen ? styles.open : ''}`}>
+        <div className={`${styles.drawer} ${styles.up} ${isOpen ? styles.open : ''}`}>
             <div className={styles.drawer__container}>
                 <button onClick={toggleDrawer} type="button" className={styles.drawer__line_container}>
                     <div className={`${styles.drawer__line} ${isOpen ? styles.drawer__line_active : styles.drawer__line_disabled}`} />
@@ -29,9 +24,9 @@ const Drawer = ({ isOpen, title, list, direction = DrawerDirection.up, toggleDra
                     <p className={styles.drawer__title}>{title}</p>
                     <button onClick={openFilter} className={styles.drawer__icon} />
                 </div>
-                <ul ref={container} className={styles.drawer__list}>
+                <ul ref={container}  className={styles.drawer__list}>
                     {list.map((card) => (
-                        <li key={card.id}>
+                        <li key={card.id} className={styles.drawer__list_item}>
                             <RestaurantCard onClick={openListItem} card={card} container={container} />
                         </li>
                     ))}
