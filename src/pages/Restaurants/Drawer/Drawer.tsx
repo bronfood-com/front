9964c02a2daa8f76314cdc99ef566fa7cc52019a
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-06 12:07:54 +0300

    fix onclick handler

--- src/pages/Restaurants/Drawer/Drawer.tsx
+++ src/pages/Restaurants/Drawer/Drawer.tsx
@@ -17,7 +17,7 @@ const Drawer = () => {
                 </button>
                 <div className={styles.drawer__title_container}>
                     <p className={styles.drawer__title}>{t('pages.restaurants.selectPlace')}</p>
-                    <button onClick={() => filter.open()} className={styles.drawer__icon} />
+                    <button onClick={filter.open} className={styles.drawer__icon} />
                 </div>
                 <ul ref={container} className={styles.drawer__list}>
                     {restaurantsFiltered.map((card) => (
