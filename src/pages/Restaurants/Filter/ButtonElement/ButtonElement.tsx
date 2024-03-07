0f5326e6bada165049d06bb17eb78a38b89e4855
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-07 21:46:37 +0300

    turn button into checkbox

--- src/pages/Restaurants/Filter/ButtonElement/ButtonElement.tsx
+++ src/pages/Restaurants/Filter/ButtonElement/ButtonElement.tsx
@@ -9,7 +9,7 @@ type ButtonElementProps = {
 const ButtonElement = (props: ButtonElementProps) => {
     return (
         <label className={`${styles.button_element} ${props.isActive ? styles.button_element_active : ''}`}>
-            <input className={styles.button_element_input} type="checkbox" defaultChecked={false} onChange={props.action}  />
+            <input className={styles.button_element_input} type="checkbox" defaultChecked={false} onChange={props.action} />
             <span className={`${styles.button_element_text} ${props.isActive ? styles.button_element_text_active : ''}`}>{props.text}</span>
         </label>
     );
