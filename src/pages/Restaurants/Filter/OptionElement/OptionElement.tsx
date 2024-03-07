0bd9aee31aec2257fdf6756bf37fab5a6c73df0b
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-07 22:05:09 +0300

    put span instead of p

--- src/pages/Restaurants/Filter/OptionElement/OptionElement.tsx
+++ src/pages/Restaurants/Filter/OptionElement/OptionElement.tsx
@@ -9,7 +9,7 @@ type OptionProps = {
 const OptionElement = (props: OptionProps) => {
     return (
         <div onClick={!props.selected ? props.onClick : undefined} className={`${styles.option} ${props.selected ? styles.option_selected : ''}`}>
-            <p className={styles.option__text}>{props.text}</p>
+            <span className={styles.option__text}>{props.text}</span>
             <button type="button" onClick={props.onClick} className={`${styles.option__icon} ${props.selected ? styles.option__icon_visible : ''}`} />
         </div>
     );
