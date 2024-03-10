9ab2d87a3d033648bd52b0ae5e0dfe4471c5324b
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-10 12:28:35 +0300

    restaurant real service

--- src/utils/api/restaurantsService/restaurantsServiceReal.ts
+++ src/utils/api/restaurantsService/restaurantsServiceReal.ts
@@ -8,7 +8,7 @@ export class RestaurantsServiceReal implements RestaurantsService {
         return localStorage.getItem('token');
     }
 
-    async getRestaurants(): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }>  {
+    async getRestaurants(): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }> {
         const token = this._getToken();
         const res = await fetch(`${API_URL}/restaurant/`, {
             method: 'GET',
@@ -21,6 +21,4 @@ export class RestaurantsServiceReal implements RestaurantsService {
         const { data } = result.data;
         return data;
     }
-
-
 }
