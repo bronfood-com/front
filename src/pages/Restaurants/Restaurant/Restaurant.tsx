d9f40d411cb4ab95d97d39d34fb2d4b09122ce68
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-12 22:45:34 +0300

    description layout

--- src/pages/Restaurants/Restaurant/Restaurant.tsx
+++ src/pages/Restaurants/Restaurant/Restaurant.tsx
@@ -2,6 +2,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import RestaurantPopup from './RestaurantPopup/RestaurantPopup';
 import { useRestaurants } from '../../../utils/hooks/useRestaurants/useRestaurants';
 import RestaurantImage from './RestaurantImage/RestaurantImage';
+import RestaurantDescription from './RestaurantDescription/RestaurantDescription';
 
 function Restaurant() {
     const params = useParams();
@@ -14,6 +15,13 @@ function Restaurant() {
     return (
         <RestaurantPopup close={close}>
             <RestaurantImage image={restaurant?.photo} />
+            <RestaurantDescription
+                name={restaurant?.name}
+                address={restaurant?.address}
+                workingTime={restaurant?.workingTime}
+                rating={restaurant?.rating}
+                reviews="(123+)"
+            />
         </RestaurantPopup>
     );
 }
