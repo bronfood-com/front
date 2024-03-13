7e8ae3f03209b3003026ab03281002d08eb9c1a9
Kikodze Viktor <vitland-x@yandex.ru>
2024-03-13 23:13:33 +0300

    feat: add restore password service

--- src/utils/api/authService.ts
+++ src/utils/api/authService.ts
@@ -16,11 +16,16 @@ export interface RegisterData {
     fullname: string;
 }
 
+export interface NewPasswordData {
+    temp_data_code: string;
+    password: string;
+    password_confirm: string;
+}
 /*
  temp_data_code: Temporary code that the server assign to the user in db during registration
  confirmation_code: 4-digit code that user shoud enter to confirm registration
  */
-export interface СonfirmPhoneData {
+export interface ConfirmPhoneData {
     temp_data_code: string;
     confirmation_code: string;
 }
@@ -36,7 +41,37 @@ export interface AuthService {
 
     register: ({ fullname, phone, password }: RegisterData) => Promise<{ status: 'success'; data: { temp_data_code: string } } | { status: 'error'; error_message: string }>;
 
-    confirmRegisterPhone: ({ temp_data_code, confirmation_code }: СonfirmRegisterPhoneData) => Promise<{ status: 'success'; data: User } | { status: 'error'; error_message: string }>;
+    confirmRegisterPhone: ({ temp_data_code, confirmation_code }: ConfirmPhoneData) => Promise<{ status: 'success'; data: User } | { status: 'error'; error_message: string }>;
+
+    changePasswordRequest: (phone: PhoneNumber) => Promise<
+        | {
+              status: 'success';
+              data: {
+                  temp_data_code: string;
+              };
+          }
+        | { status: 'error'; error_message: string }
+    >;
+
+    setNewPassword: ({ password, password_confirm, temp_data_code }: NewPasswordData) => Promise<
+        | {
+              status: 'success';
+              data: {
+                  temp_data_code: string;
+              };
+          }
+        | { status: 'error'; error_message: string }
+    >;
+
+    confirmChangePassword: ({ temp_data_code, confirmation_code }: ConfirmPhoneData) => Promise<
+        | {
+              status: 'success';
+              data: {
+                  message: 'Password updated';
+              };
+          }
+        | { status: 'error'; error_message: string }
+    >;
 
     logOut: () => Promise<void>;
 }
