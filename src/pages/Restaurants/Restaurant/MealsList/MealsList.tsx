fe78dd3a22cb1ce2107f5214fb2bf1910fe6ff23
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-16 10:34:25 +0300

    meals list layout

--- src/pages/Restaurants/Restaurant/MealsList/MealsList.tsx
+++ src/pages/Restaurants/Restaurant/MealsList/MealsList.tsx
@@ -1,12 +1,13 @@
 import styles from './MealsList.module.scss';
 import { Link } from 'react-router-dom';
 import BoxFood from '../BoxFood/BoxFood';
+import { Meal } from '../../../../utils/api/restaurantsService/restaurantsService';
 
-const MealsList = ({ meals }) => {
+const MealsList = ({ meals }: {meals: Meal[]}) => {
     return (
         <ul className={styles.meals_list}>
             {meals.map((meal) => (
-                <li key={meal.id} className={styles.meals_list_item}>
+                <li key={meal.id}>
                     <Link to={`/restaurants/${meal.id}`}>
                         <BoxFood card={meal} />
                     </Link>
