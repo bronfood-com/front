4d975153146d863241cb7b23ab3a0325e98acc13
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-03-29 13:18:04 +0300

    move basket context provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import './i18n.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext.tsx';
 import { RestaurantsProvider } from './contexts/RestaurantsContext.tsx';
+import { BasketProvider } from './contexts/BasketContext.tsx';
 
 const router = createBrowserRouter([{ path: '*', element: <App /> }], { basename: `${process.env.NODE_ENV === 'production' ? '/front/' : ''}` });
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <CurrentUserProvider>
             <RestaurantsProvider>
-                <RouterProvider router={router} />
+                <BasketProvider>
+                    <RouterProvider router={router} />
+                </BasketProvider>
             </RestaurantsProvider>
         </CurrentUserProvider>
     </React.StrictMode>,
