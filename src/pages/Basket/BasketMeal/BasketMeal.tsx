05eecfcc7b06436a050e6a0efe9218a256fad1c5
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-02 15:28:08 +0300

    fix meal count display

--- src/pages/Basket/BasketMeal/BasketMeal.tsx
+++ src/pages/Basket/BasketMeal/BasketMeal.tsx
@@ -1,20 +1,21 @@
 import styles from './BasketMeal.module.scss';
 import Counter from '../../../components/Counter/Counter';
-import { Meal } from '../../../utils/api/basketService/basketService';
+import { MealInBasket } from '../../../utils/api/basketService/basketService';
 import { useBasket } from '../../../utils/hooks/useBasket/useBasket';
 
-function BasketMeal({ meal }: { meal: Meal }) {
+function BasketMeal({ meal }: { meal: MealInBasket }) {
+    const {photo, name, price} = meal.meal;
     const { addMeal, deleteMeal } = useBasket();
     return (
         <div className={`${styles.basket_meal}`}>
             <div className={styles.basket_meal__container}>
-                <div className={styles.basket_meal__image} style={{ backgroundImage: `url(${meal.photo})` }} />
+                <div className={styles.basket_meal__image} style={{ backgroundImage: `url(${photo})` }} />
                 <div className={styles.basket_meal__description}>
-                    <p className={styles.basket_meal__name}>{meal.name}</p>
-                    <span className={styles.basket_meal__price}>{`${meal.price.toFixed(0)} ₸`}</span>
+                    <p className={styles.basket_meal__name}>{name}</p>
+                    <span className={styles.basket_meal__price}>{`${price.toFixed(0)} ₸`}</span>
                 </div>
                 <div className={styles.basket_meal__counter}>
-                    <Counter count={meal.quantity} increment={() => addMeal(meal)} decrement={() => deleteMeal(meal)} />
+                    <Counter count={meal.count} increment={() => addMeal(meal)} decrement={() => deleteMeal(meal)} />
                 </div>
             </div>
         </div>
--- src/pages/Basket/BasketMealsList/BasketMealsList.tsx
+++ src/pages/Basket/BasketMealsList/BasketMealsList.tsx
@@ -7,7 +7,7 @@ const BasketMealsList = ({ meals }: { meals: MealInBasket[] }) => {
         <ul className={styles.basket_meals_list}>
             {meals.map((meal) => (
                 <li key={meal.meal.id}>
-                    <BasketMeal meal={meal.meal} />
+                    <BasketMeal meal={meal} />
                 </li>
             ))}
         </ul>
