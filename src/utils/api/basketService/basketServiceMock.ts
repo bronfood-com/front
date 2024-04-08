b97aedba519161e11ab3cf71f01dc9f3b46f6a6e
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-08 20:13:02 +0300

    make basket private property in BasketServiceMock class

--- src/utils/api/basketService/basketServiceMock.ts
+++ src/utils/api/basketService/basketServiceMock.ts
@@ -2,12 +2,11 @@ import { BasketService, Basket } from './basketService';
 import { Meal, Restaurant } from '../restaurantsService/restaurantsService';
 import { mockRestaurants } from '../../../pages/Restaurants/MockRestaurantsList';
 
-const basket: Basket = {
-    restaurant: {},
-    meals: [],
-};
-
 export class BasketServiceMock implements BasketService {
+    private basket: Basket = {
+        restaurant: {},
+        meals: [],
+    };
     async _wait(ms: number) {
         return new Promise((res) => setTimeout(res, ms));
     }
@@ -19,22 +18,22 @@ export class BasketServiceMock implements BasketService {
                 return restaurant;
             }
         });
-        const hasRestaurantChanged = restaurantFound && basket.restaurant.id !== restaurantFound.id;
+        const hasRestaurantChanged = restaurantFound && this.basket.restaurant.id !== restaurantFound.id;
         const mealFound = restaurantFound && restaurantFound.meals.find((meal: Meal) => meal.id === mealId);
         if (mealFound) {
-            basket.restaurant = restaurantFound;
-            basket.meals =
-                basket.meals.length === 0 || hasRestaurantChanged
+            this.basket.restaurant = restaurantFound;
+            this.basket.meals =
+                this.basket.meals.length === 0 || hasRestaurantChanged
                     ? [{ meal: mealFound, count: 1 }]
-                    : basket.meals.some(({ meal }) => meal.id === mealFound.id)
-                      ? basket.meals.map(({ meal, count }) => {
+                    : this.basket.meals.some(({ meal }) => meal.id === mealFound.id)
+                      ? this.basket.meals.map(({ meal, count }) => {
                             if (meal.id === mealFound.id) {
                                 return { meal, count: count + 1 };
                             } else {
                                 return { meal, count };
                             }
                         })
-                      : [...basket.meals, { meal: mealFound, count: 1 }];
+                      : [...this.basket.meals, { meal: mealFound, count: 1 }];
             return { status: 'success', data: mealFound.id };
         } else {
             return { status: 'error', error_message: 'error' };
@@ -42,9 +41,9 @@ export class BasketServiceMock implements BasketService {
     }
     async deleteMeal(mealId: string): Promise<{ status: 'success'; data: string } | { status: 'error'; error_message: string }> {
         await this._wait(500);
-        const mealFound = basket.meals.find(({ meal }) => meal.id === mealId);
+        const mealFound = this.basket.meals.find(({ meal }) => meal.id === mealId);
         if (mealFound) {
-            basket.meals = basket.meals.map(({ meal, count }) => {
+            this.basket.meals = this.basket.meals.map(({ meal, count }) => {
                 if (meal.id === mealFound.meal.id && count >= 1) {
                     return { meal, count: count - 1 };
                 } else {
@@ -60,18 +59,18 @@ export class BasketServiceMock implements BasketService {
         await this._wait(500);
         const success = true;
         if (success) {
-            return { status: 'success', data: basket };
+            return { status: 'success', data: this.basket };
         } else {
             return { status: 'error', error_message: 'error' };
         }
     }
     async emptyBasket(): Promise<{ status: 'success'; data: Basket } | { status: 'error'; error_message: string }> {
         await this._wait(500);
-        basket.restaurant = {};
-        basket.meals = [];
+        this.basket.restaurant = {};
+        this.basket.meals = [];
         const success = true;
         if (success) {
-            return { status: 'success', data: basket };
+            return { status: 'success', data: this.basket };
         } else {
             return { status: 'error', error_message: 'error' };
         }
