6eeb0010f84386ad6df8b5d6e1ffc576069e6dad
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-08 21:33:21 +0300

    add docs to types

--- src/utils/api/basketService/basketService.ts
+++ src/utils/api/basketService/basketService.ts
@@ -2,12 +2,24 @@ import { BasketServiceMock } from './basketServiceMock';
 import { Meal, Restaurant } from '../restaurantsService/restaurantsService';
 
 export type MealInBasket = {
+    /**
+     * Meal in basket
+     */
     meal: Meal;
+    /**
+     * Quantity of meal in basket
+     */
     count: number;
 };
 
 export type Basket = {
+    /**
+     * Restaurant which meals are in basket
+     */
     restaurant: Restaurant | Record<string, never>;
+    /**
+     * List of meals in basket
+     */
     meals: MealInBasket[];
 };
 
