931dd92e724ea17b1f577cd46701cafc0d24d082
cipher24 <mediumAndrew@yandex.ru>
2024-04-09 13:31:53 +0300

    apply  authServiceReal

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -5,4 +5,5 @@
 export const regexClientName: RegExp = /^([a-zA-Z\\-]+(?:\s[a-zA-Z\\-]+)|[a-яА-ЯёЁ\\-]+(?:\s[a-яА-ЯёЁ\\-]+)*|[a-яА-ЯёЁ-ӘҒҚҢӨҰҮІі]+(?:\s[a-яА-ЯёЁ-ӘҒҚҢӨҰҮІі]+)*)$/;
 export const regexPassword: RegExp = /^[A-Za-z\d!@#$%^&*()-_+=<>?]{4,20}$/;
 export const regexPhoneNumberKazakhstan: RegExp = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
-export const API_URL = 'http://127.0.0.1:8000';
+// export const API_URL = 'http://127.0.0.1:8000';
+export const API_URL = 'http://45.82.14.98:8000';
