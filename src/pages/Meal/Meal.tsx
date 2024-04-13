67df48d40b5b66717562e7c15bcbf9d6cff68127
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-13 15:18:09 +0300

    use watch method to collect input values

--- src/pages/Meal/Meal.tsx
+++ src/pages/Meal/Meal.tsx
@@ -8,15 +8,22 @@ import MealDescription from './MealDescription/MealDescription';
 import MealTotal from './MealTotal/MealTotal';
 import MealFeatureList from './MealFeatureList/MealFeatureList';
 import Preloader from '../../components/Preloader/Preloader';
+import { useEffect, useState } from 'react';
 
 function Meal() {
+    const [features, setFeatures] = useState([]);
     const navigate = useNavigate();
     const params = useParams();
     const { restaurantsFiltered } = useRestaurants();
     const { addMeal, isLoading } = useBasket();
     const methods = useForm();
+    const {watch} = methods;
     const restaurant: RestaurantProps | undefined = restaurantsFiltered.find((restaurant) => restaurant.id === params.restaurantId);
     const meal = restaurant.meals.find((meal) => meal.id === params.mealId);
+    const featuresPrice = features.reduce((acc, current) => {
+        const selectedChoice = current.choices.find((choice) => choice.default === true);
+        return acc + selectedChoice.price
+    }, 0);
     const goBack = () => {
         navigate(`/restaurants/${params.restaurantId}`);
     };
@@ -27,6 +34,27 @@ function Meal() {
         await addMeal(meal.id);
         goBack();
     };
+    useEffect(() => {
+        const formValues = watch((value, { name }) => {
+            const nextFeatures = features.map((feature) => {
+                if (feature.name === name) {
+                    const choices = feature.choices.map((choice) => {
+                        if (choice.name === value[name]) {
+                            return {...choice, default: true }
+                        } else return {...choice, default: false }
+                    })
+                    return {...feature, choices}
+                } else return feature
+            });
+            setFeatures(nextFeatures);
+        })
+        return () => formValues.unsubscribe()
+    }, [watch, features]);
+
+    useEffect(() => {
+        setFeatures(meal.features)
+    }, [meal]);
+
     if (meal) {
         return (
             <FormProvider {...methods}>
@@ -35,7 +63,7 @@ function Meal() {
                         <MealImage image={meal.photo} />
                         <MealDescription name={meal.name} description={meal.description} />
                         <MealFeatureList features={meal.features} />
-                        <MealTotal price={200} />
+                        <MealTotal price={meal.price + featuresPrice} />
                         {isLoading && <Preloader />}
                     </MealPopup>
                 </form>
