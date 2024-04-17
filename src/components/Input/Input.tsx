cc1d1c76fe05ad702dd824d6bdafdb8d84fb8c93
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-17 17:38:56 +0300

    initialize state variable with empty string when  props value is undefined

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -44,7 +44,7 @@ interface Input {
 }
 
 const Input: FC<Input> = (props) => {
-    const [inputValue, setInputValue] = useState(props.value || '');
+    const [inputValue, setInputValue] = useState(props.value === undefined ? '' : props.value);
     const { t } = useTranslation();
     const errorMessage = (props.errors[props.name]?.message as string) || undefined;
     const id = useId();
