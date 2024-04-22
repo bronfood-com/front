c92ad18f13be2663327ce803063364aec7c71cbc
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-22 12:09:25 +0300

    move id postfix variable to BasketMealsList

--- src/utils/api/basketService/basketServiceMock.ts
+++ src/utils/api/basketService/basketServiceMock.ts
@@ -18,23 +18,15 @@ export class BasketServiceMock implements BasketService {
                 return restaurant;
             }
         });
-        const idPostfix = features
-            .map((feature) => {
-                const choice = feature.choices.find((choice) => choice.chosen || choice.default);
-                if (choice) {
-                    return choice.name;
-                }
-            })
-            .toString();
         const hasRestaurantChanged = restaurantFound && this.basket.restaurant.id !== restaurantFound.id;
-        const mealFoundInBasket = this.basket.meals.find(({ meal }) => meal.id === mealId || meal.id === mealId + idPostfix);
+        const mealFoundInBasket = this.basket.meals.find(({ meal }) => meal.id === mealId || meal.id === mealId);
         const mealFoundInRestaurants = restaurantFound && restaurantFound.meals.find((meal: Meal) => meal.id === mealId);
         if (mealFoundInBasket) {
             if (JSON.stringify(mealFoundInBasket?.meal.features) !== JSON.stringify(features)) {
-                this.basket.meals = [...this.basket.meals, { meal: { ...mealFoundInBasket.meal, features, id: mealFoundInBasket.meal.id + idPostfix }, count: 1 }];
+                this.basket.meals = [...this.basket.meals, { meal: { ...mealFoundInBasket.meal, features, id: mealFoundInBasket.meal.id }, count: 1 }];
             } else {
                 this.basket.meals = this.basket.meals.map(({ meal, count }) => {
-                    if (meal.id === mealId || meal.id === mealId + idPostfix) {
+                    if (meal.id === mealId || meal.id === mealId) {
                         return { meal, count: count + 1 };
                     } else {
                         return { meal, count };
@@ -46,12 +38,12 @@ export class BasketServiceMock implements BasketService {
             if (this.basket.meals.length === 0 || hasRestaurantChanged) {
                 this.basket.restaurant = restaurantFound;
                 if (features.length > 0) {
-                    this.basket.meals = [{ meal: { ...mealFoundInRestaurants, features, id: mealFoundInRestaurants.id + idPostfix }, count: 1 }];
+                    this.basket.meals = [{ meal: { ...mealFoundInRestaurants, features, id: mealFoundInRestaurants.id }, count: 1 }];
                 } else {
                     this.basket.meals = [{ meal: mealFoundInRestaurants, count: 1 }];
                 }
             } else {
-                this.basket.meals = [...this.basket.meals, { meal: { ...mealFoundInRestaurants, features, id: mealFoundInRestaurants.id + idPostfix }, count: 1 }];
+                this.basket.meals = [...this.basket.meals, { meal: { ...mealFoundInRestaurants, features, id: mealFoundInRestaurants.id }, count: 1 }];
             }
             return { status: 'success', data: this.basket };
         } else {
