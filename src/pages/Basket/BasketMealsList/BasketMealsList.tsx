a2bf2139306eafc91414819979196f57b4d23106
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-22 12:10:04 +0300

    move id postfix variable to BasketMealsList

--- src/pages/Basket/BasketMealsList/BasketMealsList.tsx
+++ src/pages/Basket/BasketMealsList/BasketMealsList.tsx
@@ -7,18 +7,18 @@ const BasketMealsList = ({ meals }: { meals: MealInBasket[] }) => {
         <ul className={styles.basket_meals_list}>
             {meals.map((meal) => {
                 const idPostfix = meal.meal.features
-                .map((feature) => {
-                    const choice = feature.choices.find((choice) => choice.chosen || choice.default);
-                    if (choice) {
-                        return choice.name;
-                    }
-                })
-                .toString();
+                    .map((feature) => {
+                        const choice = feature.choices.find((choice) => choice.chosen || choice.default);
+                        if (choice) {
+                            return choice.name;
+                        }
+                    })
+                    .toString();
                 return (
                     <li key={meal.meal.id + idPostfix}>
                         <BasketMeal mealInBasket={meal} />
                     </li>
-                )
+                );
             })}
         </ul>
     );
