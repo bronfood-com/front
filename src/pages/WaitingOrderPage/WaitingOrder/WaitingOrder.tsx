c0d528197907c9fc44f9877fbf31b56d0f108d05
Dmitry <mityourik@gmail.com>
2024-04-25 07:46:17 +0300

    fix confirmPopup by removing promise

--- src/pages/WaitingOrderPage/WaitingOrder/WaitingOrder.tsx
+++ src/pages/WaitingOrderPage/WaitingOrder/WaitingOrder.tsx
@@ -74,12 +74,7 @@ const WaitingOrder: FC = () => {
             </Modal>
             {showConfirmationPopup && (
                 <div className={styles.confirmationPopup__wrapper}>
-                    <ConfirmationPopup
-                        title={t('components.confirmationPopup.areYouSureYouWantToCancelTheOrder')}
-                        confirmButtonText={t('components.confirmationPopup.yes')}
-                        onCancel={() => setShowConfirmationPopup(false)}
-                        onSubmit={handleConfirmCancellOrder}
-                    />
+                    <ConfirmationPopup title={t('components.confirmationPopup.areYouSureYouWantToCancelTheOrder')} confirmButtonText={t('components.confirmationPopup.yes')} onCancel={() => setShowConfirmationPopup(false)} onSubmit={handleConfirmCancellOrder} />
                 </div>
             )}
             {showOrderCancelledPopup && <PopupOrderCancelled />}
