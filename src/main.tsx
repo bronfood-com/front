5b743aa48c086387ac8adb7bdc4894ad3450af65
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-25 14:10:16 +0300

    use hashRouter in production

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 import './i18n.tsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext.tsx';
 import { RestaurantsProvider } from './contexts/RestaurantsContext.tsx';
 import { BasketProvider } from './contexts/BasketContext.tsx';
 
-const router = createBrowserRouter([{ path: '*', element: <App /> }], { basename: `${process.env.NODE_ENV === 'production' ? '/front/' : ''}` });
+const browserRouter = createBrowserRouter([{ path: '*', element: <App /> }], { basename: `${process.env.NODE_ENV === 'production' ? '/front/' : ''}` });
+const hashRouter = createHashRouter([{ path: '*', element: <App /> }], { basename: `${process.env.NODE_ENV === 'production' ? '/front/' : ''}` });
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <CurrentUserProvider>
             <RestaurantsProvider>
                 <BasketProvider>
-                    <RouterProvider router={router} />
+                    <RouterProvider router={process.env.NODE_ENV === 'production' ? hashRouter : browserRouter} />
                 </BasketProvider>
             </RestaurantsProvider>
         </CurrentUserProvider>
