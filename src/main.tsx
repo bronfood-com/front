1710f022ba6491bce4bffad4bdeac7ccf2c38f86
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-26 21:39:39 +0300

    provide query client to App

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,19 @@ import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext.tsx';
 import { RestaurantsProvider } from './contexts/RestaurantsContext.tsx';
 import { BasketProvider } from './contexts/BasketContext.tsx';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const router = createHashRouter([{ path: '*', element: <App /> }]);
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <CurrentUserProvider>
             <RestaurantsProvider>
                 <BasketProvider>
-                    <RouterProvider router={router} />
+                    <QueryClientProvider client={queryClient}>
+                        <RouterProvider router={router} />
+                    </QueryClientProvider>
                 </BasketProvider>
             </RestaurantsProvider>
         </CurrentUserProvider>
