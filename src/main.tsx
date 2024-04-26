d7c12f11d9968c9289c8ab7543b85bed99988769
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-26 10:19:41 +0300

    remove browser router

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 import './i18n.tsx';
-import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext.tsx';
 import { RestaurantsProvider } from './contexts/RestaurantsContext.tsx';
 import { BasketProvider } from './contexts/BasketContext.tsx';
 
-const browserRouter = createBrowserRouter([{ path: '*', element: <App /> }]);
-const hashRouter = createHashRouter([{ path: '*', element: <App /> }]);
+const router = createHashRouter([{ path: '*', element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <CurrentUserProvider>
             <RestaurantsProvider>
                 <BasketProvider>
-                    <RouterProvider router={process.env.NODE_ENV === 'production' ? hashRouter : browserRouter} />
+                    <RouterProvider router={router} />
                 </BasketProvider>
             </RestaurantsProvider>
         </CurrentUserProvider>
