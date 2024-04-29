ff32dd7fd87d4e2532ce3ceda61c578df447fee5
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-29 14:50:47 +0300

    add jsdoc

--- src/utils/hooks/useEsc/useEsc.tsx
+++ src/utils/hooks/useEsc/useEsc.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 
+/**
+ * Invokes 'handleFunction' when user clicks on 'Esc'
+ *
+ * @param {} handleFunction
+ */
 export const useEsc = (handleFunction: () => void) => {
     useEffect(() => {
         const handleCloseByEsc = (e: KeyboardEvent) => (e.key === 'Escape' || e.key === 'Esc') && handleFunction();
