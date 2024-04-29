0d1ffa167350d585386d5f4c9a2ebf57b71b28c3
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-04-29 17:59:04 +0300

    fix typescript error

--- src/utils/api/restaurantsService/restaurantsServiceMock.ts
+++ src/utils/api/restaurantsService/restaurantsServiceMock.ts
@@ -8,13 +8,11 @@ export class RestaurantsServiceMock implements RestaurantsService {
 
     async getRestaurants(): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }> {
         await this._wait(1000);
-        return new Promise((resolve, reject) => {
-            const token = true;
-            if (token) {
-                resolve({ status: 'success', data: mockRestaurants });
-            } else {
-                reject({ status: 'error', error_message: 'error' });
-            }
-        });
+        const token = true;
+        if (token) {
+            return { status: 'success', data: mockRestaurants };
+        } else {
+            throw new Error({ status: 'error', error_message: 'error' });
+        }
     }
 }
