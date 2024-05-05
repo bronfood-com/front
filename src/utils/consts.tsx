05be98806c2f04e7ab1f69740198dc2921ec8ca9
olga_k_qschool <ms.olgasmith.jj@mail.ru>
2024-05-05 22:12:56 +0300

    add pass errors

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -3,7 +3,7 @@
  * includes dash, * only one space after words, * not space in the end
  */
 export const regexClientName: RegExp = /^([a-zA-Z\\-]+(?:\s[a-zA-Z\\-]+)|[a-яА-ЯёЁ\\-]+(?:\s[a-яА-ЯёЁ\\-]+)*|[a-яА-ЯёЁ-ӘҒҚҢӨҰҮІі]+(?:\s[a-яА-ЯёЁ-ӘҒҚҢӨҰҮІі]+)*)$/;
-export const regexPassword: RegExp = /^[A-Za-z\d!@#$%^&*()-_+=<>?]{4,20}$/;
+export const regexPassword: RegExp = /^[A-Za-z\d!@#$%^&*()-_+=<>?]$/;
 export const regexPhoneNumberKazakhstan: RegExp = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
 //export const API_URL = 'http://127.0.0.1:8000';
 export const API_URL = 'http://45.82.14.98:8000';
