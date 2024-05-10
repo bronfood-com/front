549f3231933f5557ce69aae64df72650e82a6962
Dmitry <mityourik@gmail.com>
2024-05-10 15:45:28 +0300

    add translation i18n to errors

--- src/utils/api/orderService/orderServiceMock.ts
+++ src/utils/api/orderService/orderServiceMock.ts
@@ -1,5 +1,6 @@
 import { OrderState } from './orderService';
 import { mockData } from '../../../pages/WaitingOrderPage/MockOrderList';
+import i18n from 'i18next';
 
 type ApiResponse<T> = {
     data: T | null;
@@ -11,26 +12,26 @@ class OrderServiceMock {
         return new Promise((resolve) => setTimeout(resolve, ms));
     };
 
-    _fetchResponse = async <T>(query: () => T | undefined, errorMessage: string): Promise<ApiResponse<T>> => {
+    _fetchResponse = async <T>(query: () => T | undefined, errorMessageKey: string): Promise<ApiResponse<T>> => {
         const result = query();
         if (result) {
             return { data: result, error: null };
         }
-        return { data: null, error: errorMessage };
+        return { data: null, error: i18n.t(errorMessageKey) };
     };
 
     fetchOrderIdByUserId = async (userId: string): Promise<ApiResponse<string>> => {
-        return this._fetchResponse(() => mockData.orders.find((order) => order.userId === userId)?.id, 'No confirmed orders found for this client');
+        return this._fetchResponse(() => mockData.orders.find((order) => order.userId === userId)?.id, 'components.waitingOrder.orderDoesNotExist');
     };
 
     fetchOrderedMealByOrderId = async (id: string): Promise<ApiResponse<OrderState>> => {
-        return this._fetchResponse(() => mockData.orders.find((order) => order.id === id), 'Order details not found');
+        return this._fetchResponse(() => mockData.orders.find((order) => order.id === id), 'components.waitingOrder.errorReceivingOrderData');
     };
 
     cancelOrder = async (id: string): Promise<ApiResponse<void>> => {
         const orderExists = mockData.orders.some((order) => order.id === id);
         if (!orderExists) {
-            return { data: null, error: 'Error canceling the order' };
+            return { data: null, error: 'components.waitingOrder.errorWhileCancellingTheOrder' };
         }
 
         return { data: null, error: null };
--- src/utils/api/orderService/orderSeviceReal.ts
+++ src/utils/api/orderService/orderSeviceReal.ts
@@ -1,5 +1,6 @@
 import { OrderState } from './orderService';
 import { API_URL } from '../../consts';
+import i18n from 'i18next';
 
 type ApiResponse<T> = {
     data: T | null;
@@ -11,10 +12,7 @@ class OrderServiceReal {
         const response = await fetch(url, options);
         if (!response.ok) {
             const errorData = await response.json();
-            const errorMessage =
-                errorData.key === 'UserIsAlreadyRegistered' // example error according to backend contract
-                    ? 'User is already registered' // example error
-                    : `Error ${response.status}`;
+            const errorMessage = i18n.t(`components.waitingOrder.${errorData.key}`);
             return { data: null, error: errorMessage };
         }
         const data: T = await response.json();
@@ -24,7 +22,7 @@ class OrderServiceReal {
     async fetchOrderIdByUserId(userId: string): Promise<ApiResponse<string>> {
         const response = await this.fetchResponse<{ id: string }[]>(`${API_URL}/orders?clientId=${userId}`);
         if (response.error || !response.data || response.data.length === 0) {
-            return { data: null, error: response.error || 'No orders found for this client' };
+            return { data: null, error: 'orderDoesNotExist' };
         }
         return { data: response.data[0].id, error: null };
     }
@@ -32,7 +30,7 @@ class OrderServiceReal {
     async fetchOrderedMealByOrderId(id: string): Promise<ApiResponse<OrderState>> {
         const response = await this.fetchResponse<OrderState[]>(`${API_URL}/orders?id=${id}`);
         if (response.error || !response.data || response.data.length === 0) {
-            return { data: null, error: response.error || 'Order details not found' };
+            return { data: null, error: 'errorReceivingOrderData' };
         }
         return { data: response.data[0], error: null };
     }
@@ -43,8 +41,13 @@ class OrderServiceReal {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ cancellationStatus: 'requested', isCancellationRequested: true }),
         };
-        return this.fetchResponse<void>(`${API_URL}/orders/${id}`, options);
+        const response = await this.fetchResponse<void>(`${API_URL}/orders/${id}`, options);
+        if (response.error) {
+            return { data: null, error: 'errorWhileCancellingTheOrder' };
+        }
+        return { data: null, error: null };
     }
+
 }
 
 export default OrderServiceReal;
