aa4e6f309ecc098e319ab79e36d93e05fafc74d0
Dmitry <mityourik@gmail.com>
2024-05-15 14:27:58 +0300

    add preloader to confirmPopup to cancelOrder

--- src/pages/WaitingOrderPage/WaitingOrder/WaitingOrder.tsx
+++ src/pages/WaitingOrderPage/WaitingOrder/WaitingOrder.tsx
@@ -86,11 +86,11 @@ const WaitingOrder: FC = () => {
             {showConfirmationPopup && (
                 <div className={styles.confirmationPopup__wrapper} onClick={handleOverlayClick}>
                     <ConfirmationPopup title={t('components.confirmationPopup.areYouSureYouWantToCancelTheOrder')} confirmButtonText={t('components.confirmationPopup.yes')} onCancel={() => setShowConfirmationPopup(false)} onSubmit={handleConfirmCancellOrder} />
-                    { isLoading &&
-                    <div className={styles.preloader__wrapper}>
-                        <Preloader />
-                    </div>
-                    }
+                    {isLoading && (
+                        <div className={styles.preloader__wrapper}>
+                            <Preloader />
+                        </div>
+                    )}
                 </div>
             )}
             {showOrderCancelledPopup && <PopupOrderCancelled />}
