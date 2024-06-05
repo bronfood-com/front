1364cc7d7b5c24d50300b74bfbe61a50b830d33f
cipher24 <mediumAndrew@yandex.ru>
2024-06-05 15:14:24 +0300

    try error, change order husky config

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import AboutUs from './components/AboutUs/AboutUs';
 import Feedback from './pages/Feedback/Feedback';
 import Search from './pages/Search/Search';
 import WaitingOrder from './pages/WaitingOrder/WaitingOrder/WaitingOrder';
-import Restaurant from './pages/Restaurants/Restaurant/Restaurant';
 
 function App() {
     const [city, setCity] = useState('');
@@ -44,7 +43,7 @@ function App() {
                 <Route path="/profile" element={<ProtectedRoute component={<Profile />} />} />
                 <Route path="/logout" element={<ProtectedRoute component={<Logout />} />} />
                 <Route path="/restaurants" element={<Restaurants />}>
-                    <Route path=":restaurantId" element={<Restaurant />} >
+                    <Route path=":restaurantId" element={<MealPage />} >
                         <Route path="meal/:mealId" element={<MealPage />} />
                     </Route>
                 </Route>
