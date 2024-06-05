b6eb93b70de3a0337eaf99f2db4780b065adfc91
olga_k_qschool <ms.olgasmith.jj@mail.ru>
2024-06-05 20:57:14 +0300

    fix

--- src/utils/api/favoritesService/favoritesService.ts
+++ src/utils/api/favoritesService/favoritesService.ts
@@ -1,11 +1,9 @@
 import { Restaurant } from '../restaurantsService/restaurantsService';
 import { FavoritesServiceMock } from './favoritesServiceMock';
-// import { FavoritesServiceReal } from './favoritesServiceReal';
 export interface FavoritesService {
     getFavorites: (userId: string) => Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }>;
     setFavorites: (restId: string) => Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }>;
     deleteFavorites: (restId: string) => Promise<{ status: 'success'; data: Restaurant[] | null } | { status: 'error'; error_message: string }>;
 }
 
-// export const favoritesService = new FavoritesServiceReal();
 export const favoritesService = new FavoritesServiceMock();
