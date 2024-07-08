7df013d1e3f5c2f8af0573adf1a978cf4d00315c
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-07-08 13:13:10 +0300

    use handleFetch in authServiceReal

--- src/utils/serviceFuncs/handleFetch.ts
+++ src/utils/serviceFuncs/handleFetch.ts
@@ -30,7 +30,6 @@ export const handleFetch = async (endpoint: string, { data, ...customOptions }: 
     if (data) {
         options.body = JSON.stringify(data);
     }
-    console.log(options);
     const res = await fetch(`${API_URL}/${endpoint}/`, options);
     if (res.status === 401) {
         localStorage.removeItem('token');
