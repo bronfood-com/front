03582884dade93ff0c527d64f58daab0bed75167
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-07-08 14:09:50 +0300

    use handleFetch in favoritesServiceReal

--- src/utils/api/favoritesService/favoritesServiceReal.ts
+++ src/utils/api/favoritesService/favoritesServiceReal.ts
@@ -1,54 +1,17 @@
-import { API_URL } from '../../consts';
 import { Restaurant } from '../restaurantsService/restaurantsService';
 import { FavoritesService } from './favoritesService';
+import { handleFetch } from '../../serviceFuncs/handleFetch';
 
 export class FavoritesServiceReal implements FavoritesService {
-    _getToken() {
-        return localStorage.getItem('token');
-    }
-
     async getFavorites(): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }> {
-        const token = this._getToken();
-        const res = await fetch(`${API_URL}/api/favorites`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                authorization: `Token ${token}`,
-            },
-        });
-        if (!res.ok) {
-            throw new Error('error');
-        }
-        return res.json();
+        return handleFetch('api/favorites');
     }
 
     async setFavorites(restId: string): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }> {
-        const token = this._getToken();
-        const res = await fetch(`${API_URL}/api/favorites/${restId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                authorization: `Token ${token}`,
-            },
-        });
-        if (!res.ok) {
-            throw new Error('error');
-        }
-        return res.json();
+        return handleFetch(`api/favorites/${restId}`, { method: 'PUT' });
     }
 
     async deleteFavorites(restId: string): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }> {
-        const token = this._getToken();
-        const res = await fetch(`${API_URL}/api/favorites/${restId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                authorization: `Token ${token}`,
-            },
-        });
-        if (!res.ok) {
-            throw new Error('error');
-        }
-        return res.json();
+        return handleFetch(`api/favorites/${restId}`, { method: 'DELETE' });
     }
 }
