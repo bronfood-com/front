0c638d0a0bd859bdc12b0849f29d3f211de6dc88
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-07-24 15:00:44 +0300

    remove get all meals service as unnecessary

--- src/utils/api/restaurantsService/restaurantsServiceReal.ts
+++ src/utils/api/restaurantsService/restaurantsServiceReal.ts
@@ -1,8 +1,7 @@
 import { handleFetch } from '../../serviceFuncs/handleFetch';
-import { Restaurant, Meal, RestaurantsService } from './restaurantsService';
+import { Restaurant, RestaurantsService } from './restaurantsService';
 
 export class RestaurantsServiceReal implements RestaurantsService {
-    private _allMealsCache: Meal[] | null = null;
     private _restaurantsCache: Restaurant[] | null = null;
 
     async getRestaurants(): Promise<{ status: 'success'; data: Restaurant[] } | { status: 'error'; error_message: string }> {
@@ -17,32 +16,20 @@ export class RestaurantsServiceReal implements RestaurantsService {
         return responseData;
     }
 
-    async getAllMeals(): Promise<Meal[]> {
-        if (this._allMealsCache !== null) {
-            return this._allMealsCache;
-        }
-        const responseData: Meal[] = await handleFetch('api/meals');
-        this._allMealsCache = responseData;
-        return this._allMealsCache;
-    }
-
-    async getRestaurantById(id: string): Promise<{ status: 'success'; data: Restaurant & { meals: Meal[] } } | { status: 'error'; error_message: string }> {
+    async getRestaurantById(id: string): Promise<{ status: 'success'; data: Restaurant } | { status: 'error'; error_message: string }> {
         if (this._restaurantsCache !== null) {
             const restaurant = this._restaurantsCache.find((r) => r.id === id);
             if (restaurant) {
                 return {
                     status: 'success',
-                    data: { ...restaurant, meals: this._allMealsCache || [] },
+                    data: restaurant,
                 };
             }
         }
         const restaurant = await handleFetch(`api/restaurant/${id}`);
-        if (this._allMealsCache === null) {
-            this._allMealsCache = await this.getAllMeals();
-        }
         return {
             status: 'success',
-            data: { ...restaurant.data, meals: this._allMealsCache },
+            data: restaurant.data,
         };
     }
 }
