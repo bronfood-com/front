1cb83e2a321051fecc20940d314186894b848f22
Dmitry <mityourik@gmail.com>
2024-07-26 00:03:17 +0300

    remove prod condition from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import svgr from 'vite-plugin-svgr';
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react(), svgr()],
+//     base: process.env.NODE_ENV === 'production' ? '/front/#/' : '',
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-// https://vitejs.dev/config/
+
+const prNumber = process.env.PULL_REQUEST_NUMBER;
+
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: process.env.NODE_ENV === 'production' ? '/front/#/' : '',
+    base: prNumber ? `/front/pr-preview/pr-${prNumber}/` : '/',
 });
