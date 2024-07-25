6f5b4f95e9c35c7c10bf44068531e4abb9edf945
Dmitry <mityourik@gmail.com>
2024-07-25 20:49:22 +0300

    back to default deploy preview

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -5,5 +5,8 @@
 export const regexClientName: RegExp = /^([a-zA-Z\\-]+(?:\s[a-zA-Z\\-]+)|[a-яА-ЯёЁ\\-]+(?:\s[a-яА-ЯёЁ\\-]+)*|[a-яА-ЯёЁ-ӘҒҚҢӨҰҮІі]+(?:\s[a-яА-ЯёЁ-ӘҒҚҢӨҰҮІі]+)*)$/;
 export const regexPassword: RegExp = /^[A-Za-z\d!@#$%^&*()-_+=<>?]{4,256}$/;
 export const regexPhoneNumberKazakhstan: RegExp = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
-export const YNDX_API_KEY = import.meta.env.VITE_YNDX_API_KEY;
-export const API_URL = import.meta.env.VITE_API_URL;
+// export const YNDX_API_KEY = import.meta.env.VITE_YNDX_API_KEY;
+// export const API_URL = import.meta.env.VITE_API_URL;
+
+export const YNDX_API_KEY = '15c31511-a1d5-4084-85c0-96cce06323bf';
+export const API_URL = 'https://bron-dev.bounceme.net';
