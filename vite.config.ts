8957076b8faf5814bc3be0b2bd1b27448a05ddd8
Dmitry <mityourik@gmail.com>
2024-07-26 10:25:32 +0300

    check old version yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import svgr from 'vite-plugin-svgr';
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [react(), svgr()],
-//     base: process.env.NODE_ENV === 'production' ? '/front/#/' : '',
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-
-const prNumber = process.env.PULL_REQUEST_NUMBER;
-
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: prNumber ? `/front/pr-preview/pr-${prNumber}/` : '/',
+    base: process.env.NODE_ENV === 'production' ? '/front/#/' : '',
 });
