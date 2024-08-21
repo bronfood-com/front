03886b398071320099d7cc83ec2978fac86f1f20
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-08-21 22:51:33 +0300

    absolute path to meal photo

--- src/pages/Restaurants/Restaurant/BoxFood/BoxFood.tsx
+++ src/pages/Restaurants/Restaurant/BoxFood/BoxFood.tsx
@@ -3,7 +3,6 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import styles from './BoxFood.module.scss';
 import Button from '../../../../components/ButtonIconOrange/ButtonIconOrange';
 import { Meal } from '../../../../utils/api/restaurantsService/restaurantsService';
-import { API_URL } from '../../../../utils/consts';
 import { useCurrentUser } from '../../../../utils/hooks/useCurrentUser/useCurretUser';
 import { useRestaurants } from '../../../../utils/hooks/useRestaurants/useRestaurants';
 import { useBasketMutations } from '../../../../utils/hooks/useBasket/useBasket';
@@ -31,7 +30,7 @@ function BoxFood({ card, setIsMealPageOpen }: { card: Meal; setIsMealPageOpen: D
     return (
         <div className={`${styles.boxfood}`} onClick={handleClick}>
             <div className={styles.boxfood__container}>
-                <div className={styles.boxfood__image} style={{ backgroundImage: `url(${API_URL}${card.photo})` }} />
+                <div className={styles.boxfood__image} style={{ backgroundImage: `url(${card.photo})` }} />
                 <div className={styles.boxfood__description}>
                     <p className={styles.boxfood__name}>{card.name}</p>
                     <span className={styles.boxfood__price}>{`${card.price} ₸`}</span>
