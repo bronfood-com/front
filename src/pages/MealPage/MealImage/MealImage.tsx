fe9b039fc83bc86fb31ca465831c8100cd064017
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-08-21 22:52:46 +0300

    absolute path to meal photo

--- src/pages/MealPage/MealImage/MealImage.tsx
+++ src/pages/MealPage/MealImage/MealImage.tsx
@@ -1,10 +1,9 @@
 import styles from './MealImage.module.scss';
-import { API_URL } from '../../../utils/consts';
 
 function MealImage({ image }: { image: string }) {
     return (
         <div className={styles.meal_image_container}>
-            <div className={styles.meal_image} style={{ backgroundImage: `url(${API_URL}${image})` }} />
+            <div className={styles.meal_image} style={{ backgroundImage: `url(${image})` }} />
         </div>
     );
 }
